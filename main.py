6f8fff98da7b34e79d1821fe2b60f32116ad4d92
JoshxB-T <joshtorrez320@gmail.com>
2025-12-10 19:26:12 -0600

    Implemented Inner Product

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
-def get_lists(choice):
-    list1 = input("Enter the first list of numbers (comma-separated): ")
-    list2 = input("Enter the second list of numbers (comma-separated): ")
-
-    list1 = [float(x) for x in list1.split(',')]
-    list2 = [float(x) for x in list2.split(',')]
-
-    if choice == '1':
-        print(f"Inner Product: {inner_product(list1, list2)}")
-    elif choice == '2':
-        print(f"Cosine Similarity: {cosine_similarity(list1, list2)}")
-    elif choice == '3':
-        print(f"L-1 Norm: {l1_norm(list1, list2)}")
-    elif choice == '4':
-        print(f"Euclidean Distance: {euclidean_distance(list1, list2)}")
-    elif choice == '5':
-        print(f"L-Infinity Norm: {linf_norm(list1, list2)}")
-    else:
-        print("Invalid choice. Try again.")
-
-
 def inner_product(list1, list2):
+    result = 0
 
     for i in range(len(list1)):
         result += list1[i] * list2[i]
@@ -45,6 +25,27 @@ def linf_norm(list1, list2):
     result = 0
     
     return result
+
+
+def get_lists(choice):
+    list1 = input("Enter the first list of numbers (comma-separated): ")
+    list2 = input("Enter the second list of numbers (comma-separated): ")
+
+    list1 = [float(x) for x in list1.split(',')]
+    list2 = [float(x) for x in list2.split(',')]
+
+    if choice == '1':
+        print(f"Inner Product: {inner_product(list1, list2)}")
+    elif choice == '2':
+        print(f"Cosine Similarity: {cosine_similarity(list1, list2)}")
+    elif choice == '3':
+        print(f"L-1 Norm: {l1_norm(list1, list2)}")
+    elif choice == '4':
+        print(f"Euclidean Distance: {euclidean_distance(list1, list2)}")
+    elif choice == '5':
+        print(f"L-Infinity Norm: {linf_norm(list1, list2)}")
+    else:
+        print("Invalid choice. Try again.")
 
 def main():
     print("480 Practice Similairty Algorithms")
